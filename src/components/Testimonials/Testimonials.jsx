054b9531960b394f594ca2b9c5fa941a22aa633b
Sahil Anil Khambal <sahilakhambal@gmail.com>
2026-08-08 11:52:58 +0530

    Update Testimonials.jsx

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,31 +1,9 @@
-// import { useEffect, useRef, useState } from "react";
 import * as MarqueeModule from "react-fast-marquee";
 import { testimonials } from "../../assets/data";
 import TestimonialCard from "./TestimonialCard";
-// import { motion, useAnimationFrame, useMotionValue } from "motion/react";
 
 const Testimonials = () => {
-  // const [contentWidth, setContentWidth] = useState(0);
-  // const speed = 0.1;
 
-  // useEffect(() => {
-  //     if(!marqueeRef.current)return;
-
-  //     setContentWidth(marqueeRef.current.scrollWidth / 2)
-  // }, [])
-
-  // const marqueeRef = useRef(null)
-  // const x = useMotionValue(0);
-
-  // useAnimationFrame((t, delta) => {
-  //     let next = x.get() - delta * speed;
-
-  //     if(next <= -contentWidth / 2){
-  //         next = 0
-  //     }
-
-  //     x.set(next)
-  // })
 
   console.log(MarqueeModule);
   console.log(TestimonialCard);
@@ -34,7 +12,7 @@ const Testimonials = () => {
   return (
     <div className="h-fit pt-32 mx-4 ">
       <h2 className="h2-text mb-8">Our Clients Words...</h2>
-    <div className="overflow-hidden h-[60vh] space-y-8">
+    <div className="overflow-hidden h-auto space-y-8">
       <div className="w-max flex gap-8 justify-center items-center animate-marquee hover:[animation-play-state:paused] transition-all ease-in-out">
         {[...testimonials, ...testimonials].map((item, index) => (
           <TestimonialCard
